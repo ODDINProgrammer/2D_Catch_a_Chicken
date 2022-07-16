ffab5447c49874b63170fd410a8ed2bad6099254
ODDINProgrammer <39472811+ODDINProgrammer@users.noreply.github.com>
2022-07-17 01:31:57 +0300

    Object spawn control
    Added a feature to control possibilities for all objects to spawn

--- Catch a chicken/Assets/Scripts/Spawner.cs
+++ Catch a chicken/Assets/Scripts/Spawner.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    //[Range(5f, 20f)] [SerializeField] private int _maxObjectCount;
     [Range(1f, 3f)] [SerializeField] private float _spawnDelay;
-    private int _currentObjectCount;
     [SerializeField] private int _objectSpawnAmount;
 
     private GameManager _gameManager;
 
-    public List<GameObject> _spawnableObjects = new List<GameObject>();
+
+    [SerializeField] private List<float> _chanceToSpawn;
+    private float _totalChance;
+    [SerializeField] private List<GameObject> _spawnableObjects;
 
     private void Awake()
     {
         _gameManager = FindObjectOfType<GameManager>();
-        StartCoroutine(Spawn(_spawnDelay));
-    }
-    private void Update()
-    {
 
+        for (int i = 0; i < _chanceToSpawn.Count; i++)
+        {
+            _totalChance += _chanceToSpawn[i];
+        }
+
+        StartCoroutine(Spawn(_spawnDelay));
 
     }
 
@@ -31,11 +34,28 @@ public class Spawner : MonoBehaviour
         {
             if (_gameManager.CurrentObjectCount < _gameManager.MaxObjectCount)
             {
-                int objectID = Random.Range(0, _spawnableObjects.Count);
-                GameObject newObject = Instantiate(_spawnableObjects[objectID], new Vector3(Random.Range(-8f, 8f), transform.position.y, 0f), transform.rotation);
-                _gameManager.CurrentObjectCount++;
+                SpawnObject();
             }
         }
         StartCoroutine(Spawn(_spawnDelay));
+    }
+
+    private int FindObjectID()
+    {
+        float random = Random.Range(0f, 1f);
+        float numForAdding = 0f;
+        int objectID = 0;
+        for (int j = 0; j < _chanceToSpawn.Count; j++)
+        {
+            if (_chanceToSpawn[j] / _totalChance + numForAdding >= random) { objectID = j; break; }
+            else { numForAdding += _chanceToSpawn[j] / _totalChance; }
+        }
+        return objectID;
+    }
+
+    private void SpawnObject()
+    {
+        GameObject newObject = Instantiate(_spawnableObjects[FindObjectID()], new Vector3(Random.Range(-8f, 8f), transform.position.y, 0f), transform.rotation);
+        _gameManager.CurrentObjectCount++;
     }
 }
