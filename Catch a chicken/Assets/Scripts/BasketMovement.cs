56eab362401171d13b2bbc950b4dc34882818ac5
ODDINProgrammer <39472811+ODDINProgrammer@users.noreply.github.com>
2022-07-22 23:37:23 +0300

    Menu
    Added menu

--- Catch a chicken/Assets/Scripts/BasketMovement.cs
+++ Catch a chicken/Assets/Scripts/BasketMovement.cs
@@ -11,8 +11,11 @@ public class BasketMovement : MonoBehaviour
     }
     private void Update()
     {
-        float posX = Input.GetAxis("Mouse X");
-        posX = Mathf.Clamp(posX, -8f, 8f);
-        transform.Translate(posX * _basketSpeed, 0f, 0f);
+        if (!MenuManager.GameIsPaused)
+        {
+            float posX = Input.GetAxis("Mouse X");
+            posX = Mathf.Clamp(posX, -8f, 8f);
+            transform.Translate(posX * _basketSpeed, 0f, 0f);
+        }
     }
 }
---
+++ Catch a chicken/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    [SerializeField] private GameObject _menu;
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    private void PauseGame()
+    {
+        GameIsPaused = !GameIsPaused;
+        if (GameIsPaused)
+        {
+            _menu.SetActive(!_menu.activeSelf);
+            Cursor.lockState = CursorLockMode.None;
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            _menu.SetActive(!_menu.activeSelf);
+            Cursor.lockState = CursorLockMode.Locked;
+            Time.timeScale = 1f;
+        }
+    }
+    public void Continue()
+    {
+        PauseGame();
+    }
+}
